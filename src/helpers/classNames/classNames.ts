9f75186e41efcdb644a1ff7488957e4b62b3a93f
saroyangor <dr.saroyan98@gmail.com>
2023-02-07 23:25:19 +0400

    init

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,12 @@
+type Mods = Record<string, boolean | string>
+
+export function classNames(cls: string = '', additional: string[] = [], mods: Mods = {}): string {
+    return [
+        cls,
+        ...additional,
+        Object.entries(mods)
+            .filter(([_, value]) => Boolean(value))
+            .map(([cls]) => cls)
+    ]
+        .join(' ')
+}
