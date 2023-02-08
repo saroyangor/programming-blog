de54fb614ff4b79d30d96347adb680571bc9e000
saroyangor <dr.saroyan98@gmail.com>
2023-02-08 18:27:52 +0400

    FSD pattern added

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,8 +1,14 @@
 import {ResolveOptions} from "webpack";
 
-export function buildResolvers(): ResolveOptions {
+import {BuildOptions} from "./types/config";
+
+export function buildResolvers(options: BuildOptions): ResolveOptions {
 
     return {
         extensions: ['.tsx', '.ts', '.js'],
+        preferAbsolute: true,
+        modules: [options.paths.src, 'node_modules'],
+        alias: {},
+        mainFiles: ['index']
     }
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import React, {Suspense} from 'react'
 import {Link, Route, Routes} from 'react-router-dom'
 
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async"
-import {MainPageAsync} from "./pages/MainPage/MainPage.async"
-import {useTheme} from "./theme/useTheme"
-import {classNames} from "./helpers/classNames/classNames"
+import {AboutPage} from "pages/AboutPage"
+import {MainPage} from "pages/MainPage"
+import {classNames} from "shared/lib/classNames/classNames"
+
+import {useTheme} from "./providers/themeProvider"
 
 import './styles/index.scss'
 
@@ -18,8 +19,8 @@ const App = () => {
             <Link to={'/about'}>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />} />
-                    <Route path={'/'} element={<MainPageAsync />} />
+                    <Route path={'/about'} element={<AboutPage />} />
+                    <Route path={'/'} element={<MainPage />} />
                 </Routes>
             </Suspense>
         </div>
