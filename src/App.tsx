9e04b23d4591eedcb1999379c53879c5426d10cd
saroyangor <dr.saroyan98@gmail.com>
2023-07-02 13:47:26 +0400

    7. Организация стилей. Добавляем темы

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ import {Link, Route, Routes} from 'react-router-dom';
 import {MainPageAsync} from "@/pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "@/pages/AboutPage/AboutPage.async";
 
-import './index.scss';
+import './styles/index.scss';
+import {useTheme} from "@/theme/useTheme";
 
 const App = () => {
+  const {theme, toggleTheme} = useTheme()
+
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to='/'>Home</Link>
       <Link to='/about'>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
