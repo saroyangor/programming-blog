823a6e9b8805ead6e5eae84b2dd1bf4b842b9ce4
saroyangor <dr.saroyan98@gmail.com>
2023-07-05 20:25:02 +0400

    30. Redux-toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import { StoreProvider } from '@/app/providers/StoreProvider';
 import { ThemeProvider } from '@/app/providers/ThemeProvider';
 import { ErrorBoundary } from '@/app/providers/ErrorBoundary';
 import '@/shared/config/i18n';
@@ -8,13 +9,14 @@ import '@/shared/config/i18n';
 import App from './app/App';
 
 render(
-  <BrowserRouter>
-    <ErrorBoundary>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </ErrorBoundary>
-
-  </BrowserRouter>,
+  <StoreProvider>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </ErrorBoundary>
+    </BrowserRouter>
+  </StoreProvider>,
   document.getElementById('root'),
 );
