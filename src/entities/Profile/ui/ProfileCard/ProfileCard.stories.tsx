42d8a5928e060c12d94a074d8c9e7e1b6bc2f3d8
saroyangor <dr.saroyan98@gmail.com>
2023-07-14 23:41:26 +0400

    46. React refresh plugin. build babel loader

--- src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -25,7 +25,6 @@ Default.args = {
     lastname: 'Saroyan',
     currency: Currency.USD,
     city: 'Yerevan',
-    avatar: 'https://pic.rutubelist.ru/user/3b/27/3b2758ad5492a76b578f7ee072e4e894.jpg',
   },
 };
 
